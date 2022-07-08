45ac5e227d3cdd8b1682fc11e876c18fd65286f8
Sven Herzberg <sven.herzberg@lemon.markets>
2022-07-08 09:43:37 +0200

    🧽 Align with Python Sample Code

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ const instruments = [
 
 liveStreaming()
 
+const spinner = '-\\|/'
+let updateCounter = 0
+let latestQuotes = {}
+
 async function liveStreaming () {
   if (process.version < 'v17.5') {
     throw Error(`Node.js introduced fetch() in version v17.5. This is node.js ${process.version}. Please update.`)
@@ -24,36 +28,64 @@ async function liveStreaming () {
   if (typeof fetch !== 'function') {
     throw Error(`Node.js versions prior to v18 need the command line option '--experimental-fetch'. This is node.js ${process.version}`)
   }
+  console.log('Fetching credentials for live streaming…')
   const response = await fetch('https://realtime.lemon.markets/v1/auth', {
     method: 'POST',
     headers: {
-      "Authorization": `Bearer ${secret}`
+      'Authorization': `Bearer ${secret}`
     }
   })
   const {user_id, token, expires_at} = await response.json()
   // **NOTE:** Use `expires_at` to reconnect, because this connection will stop
   // receiving data.
-  console.log("token expires at:", Date(expires_at))
+  console.log('token expires at:', Date(expires_at))
 
+  console.log('Fetched. Connecting Ably client…')
   const connection = new Realtime({
     token: token,
     transportParams: { remainPresentFor: 1000 }
   })
 
+  console.log(`Connected. Subscribing to ${user_id}…`)
   const channelQuotes = connection.channels.get(user_id)
   channelQuotes.subscribe((message) => {
     const {name, data} = message
     switch (name) {
     case 'quotes':
-      console.log('quotes', data)
+      latestQuotes[data.isin] = data
+      updateCounter += 1
+      printQuotes()
       break
     default:
       console.log('unexpected message:', name, data)
     }
   })
+
+  console.log(`Subscribed. Publishing requested instruments to ${user_id}.subscriptions…`)
   const channelSubscriptions = connection.channels.get(`${user_id}.subscriptions`)
   channelSubscriptions.publish('isins', instruments.join(','), (err) => {
-    console.log('publish subscriptions error', err)
+      if (err) {
+          console.log('publish subscriptions error', err)
+      }
   })
-  console.log('awaiting messages…')
+  console.log('Published. Fetching latest quotes for initialization…')
+  const quotes = await fetch('https://data.lemon.markets/v1/quotes/latest?decimals=false&epoch=true&isin=' + instruments.join(','), {
+    headers: {
+      'Authorization': `Bearer ${secret}`
+    }
+  })
+  for (const quote of (await quotes.json()).results) {
+      latestQuotes[quote.isin] = quote
+  }
+  printQuotes()
+}
+
+function printQuotes () {
+  process.stdout.write('\r')
+    for (const instrument of instruments) {
+        const quote = latestQuotes[instrument]
+        const date = new Date(quote.t)
+        process.stdout.write(`${instrument}:ask=${quote.a},bid=${quote.b},date=${date.toISOString()} `)
+    }
+  process.stdout.write(spinner[updateCounter % spinner.length])
 }
--- readme.md
+++ readme.md
@@ -4,6 +4,25 @@ This is a small example about using the [live streaming API](https://docs.lemon.
 
 ## Quick Start
 
+**NOTE:** This sample project works best with node.js v18+.
+
 1. Open `index.js` and replace `YOUR-API-KEY` with your paper trading API key
 2. Install the Ably SDK via `npm install`
 3. Run the code via `node index.js`
+
+You should see something like this:
+
+```
+Fetching credentials for live streaming…
+(node:31759) ExperimentalWarning: The Fetch API is an experimental feature. This feature could change at any time
+(Use `node --trace-warnings ...` to show where the warning was created)
+token expires at: Fri Jul 08 2022 09:41:17 GMT+0200 (Central European Summer Time)
+Fetched. Connecting Ably client…
+09:41:17.009 Ably: Auth(): Warning: library initialized with a token literal without any way to renew the token when it expires (no authUrl, authCallback, or key). See https://help.ably.io/error/40171 for help
+Connected. Subscribing to usr_qyJDQss5546j0bXjtWtLlRC3B4CNBdmg9V…
+Subscribed. Publishing requested instruments to usr_qyJDQss5546j0bXjtWtLlRC3B4CNBdmg9V.subscriptions…
+Published. Fetching latest quotes for initialization…
+US64110L1061:ask=1868400,bid=1863600,date=2022-07-08T07:41:11.000Z US88160R1014:ask=7252000,bid=7235000,date=2022-07-08T07:41:19.300Z -
+```
+
+If you pay close attention, you will notice updates to the data in the bottom line as your code receives new updates.
